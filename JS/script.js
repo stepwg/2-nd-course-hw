7ba32c2bb1efa81603177a137aa2c7825971598a
stepwg <stepwggg@gmail.com>
2023-12-29 19:43:50 +0300

    hw4

--- JS/script.js
+++ JS/script.js
@@ -172,3 +172,60 @@ else if ((clientOS === 0) && (clientDeviceYear >= 2015)) {
 else {
     console.log("Устновите облегченную версиб приложения для Иос по ссылке");
 }
+
+
+// дз 2.4
+
+for (let i = 0; i < 2; i++) {
+    console.log("Привет");
+}
+
+
+
+
+for(let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+
+
+
+for(let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+
+
+
+
+
+let obj = {
+    "Коля": "200",
+    "Вася": "300",
+    "Петя": "400",
+};
+
+for(let key in obj) {
+    console.log("${key} - зарплата $(obj[key]) долларов");
+}
+
+
+
+
+
+let n = 1000;
+let numb = 0;
+
+while(n >= 50) {
+    n /= 2;
+    numb++;
+}
+console.log(n);
+console.log(numb);
+
+
+
+let firstFriday = 5;
+for(let i = firstFriday; i <= 31; i += 7) {
+    console.log("сегодня пятница, ${i} -е число. необходимо подготовить отчет");
+}
